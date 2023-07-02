0d9fb662388c8fa0ddbc50e03dfac5fe686b11b2
borodkoleg <borodkoleg@gmail.com>
2023-07-02 15:33:00 +0300

    get_template_part

--- wp-content/themes/genius_my/header.php
+++ wp-content/themes/genius_my/header.php
@@ -21,6 +21,9 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<?php get_template_part('parts/part')?>
+
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'genius_my' ); ?></a>
