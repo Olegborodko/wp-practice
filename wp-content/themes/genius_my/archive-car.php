03e20b7861b46c774e8c3c7bda8f8da36c3027b2
borodkoleg <borodkoleg@gmail.com>
2023-07-31 17:59:09 +0300

    single-car.php

--- wp-content/themes/genius_my/archive-car.php
+++ wp-content/themes/genius_my/archive-car.php
@@ -1,1 +1,29 @@
-template for post type - car
+<?php
+
+if (have_posts()) {
+  ?>
+  <header class="page-header">
+    <?php
+    the_archive_title('<h1 class="page-title">', '</h1>');
+    the_archive_description('<div class="archive-description">', '</div>');
+    ?>
+  </header>
+  
+  <?php
+  while (have_posts()) {
+    the_post();
+    echo "ID=> " . get_the_ID();
+    echo "<br>";
+    echo "Title=> " . get_the_title();
+    echo "<br>";
+    ?>
+
+    <a href="<?= get_the_permalink() ?>">
+      Read More
+    </a>
+    <?php
+    echo "<br>";
+    echo "============";
+    echo "<br>";
+  }
+}
