6ec2d0908a08c1400fde2a17d7e2fe6a9528edd7
borodkoleg <borodkoleg@gmail.com>
2023-11-05 23:33:40 +0300

    custom pagination

--- wp-content/themes/genius_my/archive-car.php
+++ wp-content/themes/genius_my/archive-car.php
@@ -8,10 +8,17 @@ if (have_posts()) {
     the_archive_description('<div class="archive-description">', '</div>');
     ?>
   </header>
-  
+
   <?php
-  while (have_posts()) {
-    the_post();
+  $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+  $cars = new WP_Query([
+    'post_type' => 'car',
+    'posts_per_page' => 2,
+    'paged' => $paged,
+  ]);
+
+  while ($cars->have_posts()) {
+    $cars->the_post();
     echo "ID=> " . get_the_ID();
     echo "<br>";
     echo "Title=> " . get_the_title();
@@ -29,5 +36,14 @@ if (have_posts()) {
 
   echo "<br/>";
   echo "<br/>";
-  echo paginate_links();
+  echo paginate_links(
+    array(
+      'base' => get_pagenum_link(1) . '%_%',
+      'format' => '/page/%#%',
+      'current' => max(1, get_query_var('paged')),
+      'total' => $cars->max_num_pages,
+      'prev_text' => __('« prev'),
+      'next_text' => __('next »'),
+    )
+  );
 }
